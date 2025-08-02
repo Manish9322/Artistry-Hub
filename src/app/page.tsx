59bfdc8ea763a717eede0d8fe19fdab7aab7933b
Manish Sonawane <manishsonawane3010@gmail.com>
2025-08-02 06:42:46 +0000

    now add fade effect in it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import Image from "next/image";
@@ -385,6 +386,8 @@ export default function Home() {
                       </div>
                     ))}
                   </div>
+                  <div className="pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r from-background to-transparent"></div>
+                  <div className="pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l from-background to-transparent"></div>
               </div>
           </div>
         </section>
