a11d8fe66008c65ae7a1aa4362e8972e76253fbc
Manish Sonawane <manishsonawane3010@gmail.com>
2025-08-02 06:44:07 +0000

    change the color of fade effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -386,8 +386,8 @@ export default function Home() {
                       </div>
                     ))}
                   </div>
-                  <div className="pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r from-background to-transparent"></div>
-                  <div className="pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l from-background to-transparent"></div>
+                  <div className="pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r from-primary/10 to-transparent"></div>
+                  <div className="pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l from-primary/10 to-transparent"></div>
               </div>
           </div>
         </section>
