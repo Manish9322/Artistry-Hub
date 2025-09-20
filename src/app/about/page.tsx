bc0c5815da178f707994ddfd76e082692b945d34
Manish Sonawane <manishsonawane3010@gmail.com>
2025-09-20 09:14:12 +0000

    in "From Our Clients' Lens" make the scrollbar invisible, but ensure its

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -324,7 +324,7 @@ export default function AboutPage() {
                   </figure>
                 ))}
               </div>
-              <ScrollBar orientation="horizontal" className="mt-4" />
+              <ScrollBar orientation="horizontal" className="hidden" />
             </ScrollArea>
              <div className="pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r from-background to-transparent"></div>
              <div className="pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l from-background to-transparent"></div>
