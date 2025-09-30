250064ded2c1981d12afa4189abd03e6c103d470
Manish Sonawane <manishsonawane3010@gmail.com>
2025-09-30 05:04:55 +0000

    s5-ugn1tho3bo7O4ZLkliLvlgz0 this is api secret, 575135343336334 this is

--- src/lib/config.js
+++ src/lib/config.js
@@ -6,3 +6,4 @@ export const EMAIL_PASS = process.env.EMAIL_PASS;
 export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
 export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
 export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
+
