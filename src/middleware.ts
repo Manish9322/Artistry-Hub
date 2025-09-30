b5539364c2c4833f6ee92ebd0540d0be8f8365ba
Manish Sonawane <manishsonawane3010@gmail.com>
2025-09-30 17:25:51 +0000

    for now do the changes to allow creating new category using psotman,
    i

--- src/middleware.ts
+++ src/middleware.ts
@@ -16,35 +16,35 @@ export async function middleware(request: NextRequest) {
   ];
   
   // All non-GET requests to the API are protected by default
-  if (request.method !== 'GET' && pathname.startsWith('/api/')) {
-    // If the route is public, allow it to pass through without a token
-    if (publicRoutes.includes(pathname)) {
-        return NextResponse.next();
-    }
+  // if (request.method !== 'GET' && pathname.startsWith('/api/')) {
+  //   // If the route is public, allow it to pass through without a token
+  //   if (publicRoutes.includes(pathname)) {
+  //       return NextResponse.next();
+  //   }
 
-    const authHeader = request.headers.get('Authorization');
+  //   const authHeader = request.headers.get('Authorization');
     
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-      return new NextResponse(
-        JSON.stringify({ success: false, message: 'Authentication required' }),
-        { status: 401, headers: { 'Content-Type': 'application/json' } }
-      );
-    }
+  //   if (!authHeader || !authHeader.startsWith('Bearer ')) {
+  //     return new NextResponse(
+  //       JSON.stringify({ success: false, message: 'Authentication required' }),
+  //       { status: 401, headers: { 'Content-Type': 'application/json' } }
+  //     );
+  //   }
 
-    const token = authHeader.split(' ')[1];
+  //   const token = authHeader.split(' ')[1];
 
-    try {
-      await jwtVerify(token, secret);
-      // Token is valid, proceed with the request
-      return NextResponse.next();
-    } catch (err) {
-      // Token is invalid
-      return new NextResponse(
-        JSON.stringify({ success: false, message: 'Invalid or expired token' }),
-        { status: 401, headers: { 'Content-Type': 'application/json' } }
-      );
-    }
-  }
+  //   try {
+  //     await jwtVerify(token, secret);
+  //     // Token is valid, proceed with the request
+  //     return NextResponse.next();
+  //   } catch (err) {
+  //     // Token is invalid
+  //     return new NextResponse(
+  //       JSON.stringify({ success: false, message: 'Invalid or expired token' }),
+  //       { status: 401, headers: { 'Content-Type': 'application/json' } }
+  //     );
+  //   }
+  // }
 
   // Allow all other requests (e.g., all GET requests) to pass through
   return NextResponse.next();
